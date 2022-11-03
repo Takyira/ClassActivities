78cb4622cda77396d055de8951e213adee4fdff8
Takyira Wilson <takyirawilson@gmail.com>
2022-11-02 21:40:12 -0500

    Up to Activity 2.2

---
+++ Section02/2.1/markdownPractice.md
@@ -0,0 +1,17 @@
+## Favorite Anime ##
+---
+#### Anime ####
+
+_Who likes Anime?_
+
+~~If anyone says no, booo~~
+
+- Bleach
+- Ouran Host Academy
+- Fruits Basket
+
+**What genre?**
+
+ 1. Slice of life
+ 2. Action
+ 3. Mystery
---
+++ Section02/2.2/2.2.1-JSVariablesAndPrimitives.js
@@ -0,0 +1,22 @@
+// create a constant variable (const)
+const myName = "Takyira";
+// create a variable that can be reassigned (let)
+let age = 25;
+// create three variables and assign them values of different data types (=)
+let favoriteColor = "purple";
+let hasCat = true;
+let numberOfArms = 2;
+let language = "JavaScript";
+// print the types of two variables that reference two different data types (typeof)
+// TIP: console.log() prints a value in the console/terminal
+console.log(myName);
+console.log(numberOfArms);
+// change the value referenced by a variable (dynamic typing)
+numberOfArms = 5;
+// print the type of the variable you just changed (typeof)
+console.log(numberOfArms);
+// create variables and give them values to complete the sentences that will print  below.
+// Which variables need to be created?
+// What type of data needs to go in each variable?
+
+console.log("Hello, my name is " + myName + ", and I am learning " +  language + ". I have been practicing for only " +  age + " weeks, so my status as a master is: " + {true or false} + ".");
